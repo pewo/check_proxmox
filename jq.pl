835a5fff9e61be4985d49d5b0eeef73d655e06d4
Peter Wirdemo <4224155+pewo@users.noreply.github.com>
2021-12-07 08:23:37 +0100

    Update jq.pl

--- jq.pl
+++ jq.pl
@@ -7,19 +7,16 @@ use Data::Dumper;
 my $json = JSON->new->allow_nonref;
 
 while ( <STDIN>  ) {
-    my $perl_scalar = $json->decode( $_ );
-    if ( ! defined($perl_scalar) ) {
-        die "Bad JSON? $_: $!\n";
+    my $perl_scalar = eval  { $json->decode( $_ )};
+    if ( $@ ) {
+	    exit;
     }
-    #print "Output: " . Dumper(\$perl_scalar) . "\n";
 
     my $new = $perl_scalar;
+
     foreach ( @ARGV ) {
         $new = $new->{$_};
     }
 
-    if ( defined($new) ) {
-        print "$new\n";
-    }
+    print "$new\n";
 }
-
